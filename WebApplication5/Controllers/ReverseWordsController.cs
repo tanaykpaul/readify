b068a0bc705cb035a686297ec4029675462b5f89
tanaykpaul <tanaykpaul@gmail.com>
2018-10-22 22:49:59 +1100

    pass1

--- WebApplication5/Controllers/ReverseWordsController.cs
+++ WebApplication5/Controllers/ReverseWordsController.cs
@@ -15,28 +15,6 @@ namespace Readify.Controllers
             return request.CreateResponse(HttpStatusCode.OK, GetReverseWords(sentence));
         }
 
-        //private static string GetReverseWords(string sentense)
-        //{
-        //    var output = string.Empty;
-        //    if (string.IsNullOrWhiteSpace(sentense)) return sentense;
-
-        //    const string pattern = @"\w(?<!\d)[\w'-]*";
-        //    var matches = new Regex(pattern).Matches(sentense);
-
-        //    var lastPosition = 0;
-        //    foreach (var matchObj in matches.Cast<Match>())
-        //    {
-        //        output = $"{output}{sentense.Substring(lastPosition, matchObj.Index - lastPosition)}{Reverse(matchObj.Value)}";
-        //        lastPosition = matchObj.Index + matchObj.Length;
-        //    }
-        //    if (lastPosition < sentense.Length)
-        //    {
-        //        output = $"{output}{sentense.Substring(lastPosition)}";
-        //    }
-
-        //    return output;
-        //}
-
         private static string GetReverseWords(string sentense)
         {
             var items = sentense.Split(' ');
