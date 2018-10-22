e656223ca563a9b1fff1ec81b56cf2d623c31198
tanaykpaul <tanaykpaul@gmail.com>
2018-10-22 15:05:22 +1100

    [check]

--- WebApplication5/Controllers/TokenController.cs
+++ WebApplication5/Controllers/TokenController.cs
@@ -11,7 +11,7 @@ namespace Readify.Controllers
                 $"My Token is {result}",
                 new Result
                 {
-                    Input = null,
+                    Input = "",
                     Output = $"{result}"
                 }
                 );
